05c66e24ba904e9192a22a49b8d41417628415df
hdcdic <hugh.ding@mail.utoronto.ca>
2020-11-28 21:23:14 -0500

    improved code to meet coding standards

--- part1_strip_html.js
+++ part1_strip_html.js
@@ -1,28 +1,63 @@
+/*
+=================================
+Part 1 - get content
+=================================
+*/
+
+// Get uploaded files and download their lists of contents
+function create_content_lists() {
+	// English
+	// read in uploaded file as string
+	let file_reader_en = new FileReader();
+	let dirty_file_en = document.getElementById("dirty_en").files[0];
+	file_reader_en.onload = function(event) {
+		// clean string
+		console.log("number of en values:");
+		let values_en = strip_html(event.target.result);
+		// download file
+		download(values_en, "en_values.txt", "text/plain");
+	}
+	file_reader_en.readAsText(dirty_file_en);
+	
+	// French
+	let file_reader_fr = new FileReader();
+	let dirty_fr_file = document.getElementById("dirty_fr").files[0];
+	file_reader_fr.onload = function(event) {
+		console.log("number of fr values:");
+		let values_fr = strip_html(event.target.result);
+		download(values_fr, "fr_values.txt", "text/plain");
+	}
+	file_reader_fr.readAsText(dirty_fr_file);
+}
+
+/* Helper functions */
+
 // formats html file and strips all of its tags to create a list of content values
 function strip_html(html_str) {
-	html_arr = html_str.split('\n')
+	let html_arr = html_str.split('\n');
 	// remove first few lines of html file
-	html_arr = html_arr.slice(6, html_arr.length)
+	html_arr = html_arr.slice(6, html_arr.length);
 	// remove logiterms
-	html_arr = html_arr.map(x => x.replaceAll(/<a name="lt_[a-zA-z0-9]+">([^<]*)<\/a>/g, "$1"))
+	html_arr = html_arr.map(x => x.replaceAll(/<a name="lt_[a-zA-z0-9]+">([^<]*)<\/a>/g, "$1"));
 	// replace special characters
-	html_arr = html_arr.map(replace_special_chars)
+	html_arr = html_arr.map(replace_special_chars);
 	// make spacings consistent
-	html_arr = html_arr.map(rm_extra_space)
+	html_arr = html_arr.map(rm_extra_space);
 	// get rid of tags
-	html_arr = html_arr.map(rm_tags_all)
+	html_arr = html_arr.map(rm_tags_all);
 	// remove indents, list numberings, and empty lines
-	html_arr = trim_arr(html_arr)
-	html_arr = html_arr.map(rm_list)
-	html_arr = rm_empty_lines(html_arr)
-	html_arr = trim_arr(html_arr)
+	html_arr = trim_arr(html_arr);
+	html_arr = html_arr.map(rm_list);
+	html_arr = rm_empty_lines(html_arr);
+	html_arr = trim_arr(html_arr);
 	// join array back into string and split it again to get actual list
-	html_str_cleaned = html_arr.join('\n')
-	html_arr = html_str_cleaned.split('\n')
+	html_str_cleaned = html_arr.join('\n');
+	html_arr = html_str_cleaned.split('\n');
 	// remove empty lines and lines that only consist of non-alphanumeric characters
-	html_arr = rm_substr(html_arr, /^[^a-zA-z0-9]+$/g)
-	html_arr = rm_empty_lines(html_arr)
-	html_arr = trim_arr(html_arr)
-	console.log(html_arr.length)
-	return html_arr.join('\n')
+	html_arr = rm_substr(html_arr, /^[^a-zA-z0-9]+$/g);
+	html_arr = rm_empty_lines(html_arr);
+	html_arr = trim_arr(html_arr);
+  // print number of elements in array
+	console.log(html_arr.length);
+	return html_arr.join('\n');
 }
--- part2_replace_en_with_fr.js
+++ part2_replace_en_with_fr.js
@@ -1,84 +1,134 @@
+const unmatchable_string_emptyline = "EMPTYLINEFILLER123456789"
+const unmatchable_string_placeholder = "FILLER123456789PLACEHOLDER"
+const script_notify = "SUPERSCRIPTORSUBSCRIPT"
+
+/*
+=================================
+Part 2 - create french structure
+=================================
+*/
+// Get lists of contents and replace English terms with French terms
+function create_fr_html() {
+	// file inputs - en content and fr content in that order
+	let content_inputs = ["", ""];
+	// read in content files as strings
+	// english content
+	let file_reader_content_en = new FileReader();
+	let content_str_en = document.getElementById("content_en").files[0];
+	file_reader_content_en.onload = function(event) {
+		content_inputs[0] = event.target.result;
+	}
+	file_reader_content_en.readAsText(content_str_en);
+    // french content
+	let file_reader_content_fr = new FileReader();
+	let content_str_fr = document.getElementById("content_fr").files[0];
+	file_reader_content_fr.onload = function(event) {
+		content_inputs[1] = event.target.result;
+	}
+	file_reader_content_fr.readAsText(content_str_fr);
+
+	// compare english and french contents
+	let file_reader_en_struct = new FileReader();
+	let struct_str_en = document.getElementById("cleaned_en").files[0];
+	file_reader_en_struct.onload = function(event) {
+		// read in english and french contents
+		const en_contents = content_inputs[0].split("\n");
+		const fr_contents = content_inputs[1].split("\n");
+		// read in english structure
+		let structure = replace_special_chars(event.target.result);
+		structure = rm_extra_space(structure);
+		// read in other inputs for matching contents to structure
+		const min_cont_len = parseInt(document.getElementById("min_content_len").value);
+		// replace english content in structure with french content
+		new_structure = replace_en_with_fr(structure, en_contents, fr_contents, min_cont_len);
+		// download structure with french content
+		download(new_structure, "fr_html.html", "text/html");
+	}
+	file_reader_en_struct.readAsText(struct_str_en);
+}
+
+/* Helper functions */
+
 // Compare content lengths
 function compare_content_len(a, b) {
 	// set all numeric substrings to length 1
-	a_nonum = a.value.replaceAll(/[0-9]+/g, "1")
-	b_nonum = b.value.replaceAll(/[0-9]+/g, "1")
+	const a_nonum = a.value.replaceAll(/[0-9]+/g, "1");
+	const b_nonum = b.value.replaceAll(/[0-9]+/g, "1");
 	// sort by substring length, then position
 	if (a_nonum.length == b_nonum.length) {
-		return b.position - a.position
+		return b.position - a.position;
 	}
-	return b_nonum.length - a_nonum.length
+	return b_nonum.length - a_nonum.length;
 }
 
 // Replace English substrings with French substrings
 function replace_en_with_fr(en_structure, en_contents, fr_contents, min_cont_len) {
-	ncontents = Math.min(en_contents.length, fr_contents.length)
-	unmatched_lines = 0
-	unmatched_excluding_placeholder = 0
-	console.log("Unmatched lines (first 100):")
+	const ncontents = Math.min(en_contents.length, fr_contents.length);
+	let unmatched_lines = 0;
+	let unmatched_excluding_placeholder = 0;
+	console.log("Unmatched lines (first 100):");
 	// lines of structure - one to return
-	struct_lines = en_structure.split("\n")
+	let struct_lines = en_structure.split("\n");
 	// one to keep track of which lines have been edited
-	struct_lines_placeholder = en_structure.split("\n")
+	let struct_lines_placeholder = en_structure.split("\n");
 	// one with superscripts and subscripts removed
-	struct_lines_no_script = en_structure.replaceAll(/<\/*su[bp]>/g, "").split("\n")
+	let struct_lines_no_script = en_structure.replaceAll(/<\/*su[bp]>/g, "").split("\n");
 	/*
 	============================
 	format en contents and convert to regex
 	============================
 	*/
 	// replace inconsistent chars
-	en_contents_orig = en_contents.map(replace_special_chars)
-	en_contents_orig = en_contents_orig.map(rm_extra_space)
-	en_contents_orig = en_contents_orig.map(x => x.replaceAll(/^ *(&nbsp;)* *$/g, ""))
+	let en_contents_orig = en_contents.map(replace_special_chars);
+	en_contents_orig = en_contents_orig.map(rm_extra_space);
+	en_contents_orig = en_contents_orig.map(x => x.replaceAll(/^ *(&nbsp;)* *$/g, ""));
 	// replace blank lines with some unmatchable string to exclude from regex
-	en_contents_orig = replace_empty_lines(en_contents_orig, unmatchable_string_emptyline)
+	en_contents_orig = replace_empty_lines(en_contents_orig, unmatchable_string_emptyline);
 	// reformat regex chars
-	en_contents_regex = en_contents_orig.map(replace_regex_chars)
+	let en_contents_regex = en_contents_orig.map(replace_regex_chars);
 	// make nbsp optional
-	en_contents_regex = en_contents_regex.map(x => x.replaceAll("&nbsp;", "(?:&nbsp;)* *"))
+	en_contents_regex = en_contents_regex.map(x => x.replaceAll("&nbsp;", "(?:&nbsp;)* *"));
 	/*
 	============================
 	sort content by length
 	============================
 	*/
-	content_len = []
-	for (i = 0; i < ncontents; i++) {
-		content_len.push({position: i, value: en_contents_regex[i]})
+	let content_len = [];
+	for (let i = 0; i < ncontents; i++) {
+		content_len.push({position: i, value: en_contents_regex[i]});
 	}
-	content_len.sort(compare_content_len)
-	console.log(content_len)
+	content_len.sort(compare_content_len);
 	// loop through english content and get its index in remaining structure
 	for (i = 0; i < ncontents; i++) {
-		posn = content_len[i].position
-		curr_content = content_len[i].value.trim()
-		curr_content_orig = en_contents_orig[posn].trim()
-		equiv_fr_content = fr_contents[posn]
-		curr_content_regex = new RegExp(curr_content, "g")
-		content_found = false
+		let posn = content_len[i].position;
+		let curr_content = content_len[i].value.trim();
+		let curr_content_orig = en_contents_orig[posn].trim();
+		let equiv_fr_content = fr_contents[posn];
+		let curr_content_regex = new RegExp(curr_content, "g");
+		let content_found = false;
 		/*
 		============================
 		actual values
 		============================
 		*/
 		// check for fully matching tag/newline/sentence first
-		newline_match = new RegExp("((^|>) *)" + curr_content + "( *($|<))", "g")
-		content_ind = regex_ind(struct_lines_placeholder, newline_match, -1)
+		let newline_match = new RegExp("((^|>) *)" + curr_content + "( *($|<))", "g");
+		let content_ind = regex_ind(struct_lines_placeholder, newline_match, -1);
 		// if match is found, change structure value and set struct counter
 		if (content_ind > -1) {
-			content_found = true
-			struct_lines[content_ind] = struct_lines[content_ind].replace(newline_match, "$1" + equiv_fr_content + "$3")
-			struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder)
+			content_found = true;
+			struct_lines[content_ind] = struct_lines[content_ind].replace(newline_match, "$1" + equiv_fr_content + "$3");
+			struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder);
 		}
 		if (curr_content.length >= min_cont_len) {
 			// check for partial match where content is found in tag/line, but tag/line doesn't consist entirely of it
-			word_match = new RegExp("(^|[^a-zA-Z0-9])" + curr_content + "($|[^a-zA-Z0-9])", "g")
-			content_ind = regex_ind(struct_lines_placeholder, word_match, content_ind)
+			let word_match = new RegExp("(^|[^a-zA-Z0-9])" + curr_content + "($|[^a-zA-Z0-9])", "g");
+			content_ind = regex_ind(struct_lines_placeholder, word_match, content_ind);
 			// if match is found, change structure value and set struct counter
 			if ((content_ind > -1) && (!content_found)) {
-				content_found = true
-				struct_lines[content_ind] = struct_lines[content_ind].replace(word_match, "$1" + equiv_fr_content + "$2")
-				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder)
+				content_found = true;
+				struct_lines[content_ind] = struct_lines[content_ind].replace(word_match, "$1" + equiv_fr_content + "$2");
+				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder);
 			}
 		}
 		/*
@@ -89,13 +139,13 @@ function replace_en_with_fr(en_structure, en_contents, fr_contents, min_cont_len
 		// only assume it's a list value if there are letters
 		if (/[a-zA-Z]/g.test(curr_content)) {
 			// check for match after list numbering is removed
-			list_match = new RegExp("((^|>) *)([0-9Ii]*[\.-][0-9Ii]* *)*" + curr_content + "( *($|<))", "g")
-			content_ind = regex_ind(struct_lines_placeholder, list_match, content_ind)
+			let list_match = new RegExp("((^|>) *)([0-9Ii]*[\.-][0-9Ii]* *)*" + curr_content + "( *($|<))", "g");
+			content_ind = regex_ind(struct_lines_placeholder, list_match, content_ind);
 			// if match is found, change structure value and set struct counter
 			if ((content_ind > -1) && (!content_found)) {
-				content_found = true
-				struct_lines[content_ind] = struct_lines[content_ind].replace(list_match, "$1" + equiv_fr_content + "$4")
-				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder)
+				content_found = true;
+				struct_lines[content_ind] = struct_lines[content_ind].replace(list_match, "$1" + equiv_fr_content + "$4");
+				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder);
 			}
 		}
 		/*
@@ -104,12 +154,12 @@ function replace_en_with_fr(en_structure, en_contents, fr_contents, min_cont_len
 		============================
 		*/
 		// check for match after sup and sub tags are removed
-		content_ind = regex_ind(struct_lines_no_script, newline_match, content_ind)
+		content_ind = regex_ind(struct_lines_no_script, newline_match, content_ind);
 		// if match is found, change structure value and set struct counter
 		if ((content_ind > -1) && (!content_found)) {
-			content_found = true
-			struct_lines[content_ind] = script_notify + struct_lines_no_script[content_ind].replace(newline_match, "$1" + equiv_fr_content + "$3")
-			struct_lines_placeholder[content_ind] = struct_lines_no_script[content_ind].replace(curr_content_regex, unmatchable_string_placeholder)
+			content_found = true;
+			struct_lines[content_ind] = script_notify + struct_lines_no_script[content_ind].replace(newline_match, "$1" + equiv_fr_content + "$3");
+			struct_lines_placeholder[content_ind] = struct_lines_no_script[content_ind].replace(curr_content_regex, unmatchable_string_placeholder);
 		}
 		/*
 		============================
@@ -118,13 +168,13 @@ function replace_en_with_fr(en_structure, en_contents, fr_contents, min_cont_len
 		*/
 		if (curr_content.length >= min_cont_len) {
 			// check for match after spacing is disregarded
-			space_match = new RegExp("((^|>) *)" + curr_content.replaceAll(" *", " ").replaceAll(" ", " *") + "( *($|<))", "g")
-			content_ind = regex_ind(struct_lines_placeholder, space_match, content_ind)
+			let space_match = new RegExp("((^|>) *)" + curr_content.replaceAll(" *", " ").replaceAll(" ", " *") + "( *($|<))", "g");
+			content_ind = regex_ind(struct_lines_placeholder, space_match, content_ind);
 			// if match is found, change structure value and set struct counter
 			if ((content_ind > -1) && (!content_found)) {
-				content_found = true
-				struct_lines[content_ind] = struct_lines[content_ind].replace(space_match, "$1" + equiv_fr_content + "$3")
-				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder)
+				content_found = true;
+				struct_lines[content_ind] = struct_lines[content_ind].replace(space_match, "$1" + equiv_fr_content + "$3");
+				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(curr_content_regex, unmatchable_string_placeholder);
 			}
 		}
 		/*
@@ -134,52 +184,52 @@ function replace_en_with_fr(en_structure, en_contents, fr_contents, min_cont_len
 		*/
 		if (curr_content.length >= min_cont_len) {
 			// check for alphanumeric matches: leading/trailing non-alphanumeric characters are removed
-			special_match_str = curr_content_orig.replace(/^[^0-9a-zA-Z]*(.*)/g, "$1").replace(/(.*)[^0-9a-zA-Z]*$/g, "$1")
+			let special_match_str = curr_content_orig.replace(/^[^0-9a-zA-Z]*(.*)/g, "$1").replace(/(.*)[^0-9a-zA-Z]*$/g, "$1");
 			// internal special characters are replaced with placeholders
-			special_match_str = special_match_str.replaceAll(/&[a-zA-Z0-9]+;/g, " ")
+			special_match_str = special_match_str.replaceAll(/&[a-zA-Z0-9]+;/g, " ");
 			// internal non-alphanumeric characters are replaced with placeholders
-			special_match_str = special_match_str.replaceAll(/[^a-zA-Z0-9]/g, " *[^a-zA-Z0-9] *")
-			special_match_str_regex = new RegExp(special_match_str)
+			special_match_str = special_match_str.replaceAll(/[^a-zA-Z0-9]/g, " *[^a-zA-Z0-9] *");
+			let special_match_str_regex = new RegExp(special_match_str);
 			// check this for fully matching tag/newline first
-			newline_special_match = new RegExp("((^|>) *)" + special_match_str + "( *($|<))", "g")
-			content_ind = regex_ind(struct_lines_placeholder, newline_special_match, content_ind)
+			let newline_special_match = new RegExp("((^|>) *)" + special_match_str + "( *($|<))", "g");
+			content_ind = regex_ind(struct_lines_placeholder, newline_special_match, content_ind);
 			// if match is found, change structure value and set struct counter
 			if ((content_ind > -1) && (!content_found)) {
-				content_found = true
-				struct_lines[content_ind] = struct_lines[content_ind].replace(newline_special_match, "$1" + equiv_fr_content + "$3")
-				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(special_match_str_regex, unmatchable_string_placeholder)
+				content_found = true;
+				struct_lines[content_ind] = struct_lines[content_ind].replace(newline_special_match, "$1" + equiv_fr_content + "$3");
+				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(special_match_str_regex, unmatchable_string_placeholder);
 			}
 			// check for partial match
-			word_special_match = new RegExp("(^|[^a-zA-Z0-9])" + special_match_str + "($|[^a-zA-Z0-9])", "g")
-			content_ind = regex_ind(struct_lines_placeholder, word_special_match, content_ind)
+			let word_special_match = new RegExp("(^|[^a-zA-Z0-9])" + special_match_str + "($|[^a-zA-Z0-9])", "g");
+			content_ind = regex_ind(struct_lines_placeholder, word_special_match, content_ind);
 			// if match is found, change structure value and set struct counter
 			if ((content_ind > -1) && (!content_found)) {
-				content_found = true
-				struct_lines[content_ind] = struct_lines[content_ind].replace(word_special_match, "$1" + equiv_fr_content + "$2")
-				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(special_match_str_regex, unmatchable_string_placeholder)
+				content_found = true;
+				struct_lines[content_ind] = struct_lines[content_ind].replace(word_special_match, "$1" + equiv_fr_content + "$2");
+				struct_lines_placeholder[content_ind] = struct_lines[content_ind].replace(special_match_str_regex, unmatchable_string_placeholder);
 			}
 		}
-		
+		;
 		// if match is found, change structure value and set struct counter
 		if (content_ind == -1) {
 			if (unmatched_lines < 100) {
-				console.log(posn + 1)
+				console.log(posn + 1);
 			}
-			unmatched_lines++
+			unmatched_lines++;
 			if (curr_content != unmatchable_string_emptyline) {
-				unmatched_excluding_placeholder++
+				unmatched_excluding_placeholder++;
 			}
 		}
 	}
-	console.log("Total unmatched lines:")
-	console.log(unmatched_lines)
-	console.log("Unmatched lines excluding placeholders:")
-	console.log(unmatched_excluding_placeholder)
+	console.log("Total unmatched lines:");
+	console.log(unmatched_lines);
+	console.log("Unmatched lines excluding placeholders:");
+	console.log(unmatched_excluding_placeholder);
 	// replace link formattings and footnotes
-	struct_lines = replace_arr(struct_lines, "/eng/", "/fra/")
-	struct_lines = replace_arr(struct_lines, "/Eng/", "/Fra/")
-	struct_lines = replace_arr(struct_lines, "Return to footnote", "Retour à la référence de la note de bas de page")
-	struct_lines = replace_arr(struct_lines, "Footnotes", "Notes de bas de page")
-	struct_lines = replace_arr(struct_lines, "Footnote", "Note de bas de page")
-	return struct_lines.join('\n')
+	struct_lines = replace_arr(struct_lines, "/eng/", "/fra/");
+	struct_lines = replace_arr(struct_lines, "/Eng/", "/Fra/");
+	struct_lines = replace_arr(struct_lines, "Return to footnote", "Retour à la référence de la note de bas de page");
+	struct_lines = replace_arr(struct_lines, "Footnotes", "Notes de bas de page");
+	struct_lines = replace_arr(struct_lines, "Footnote", "Note de bas de page");
+	return struct_lines.join('\n');
 }
